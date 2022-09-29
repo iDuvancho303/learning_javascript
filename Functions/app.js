48efeea5acbf21c49088a1fcd3ec0f40c6cf5895
Arcila Flores <DUVAN_ARCILA-FLORES@dpsk12.org>
2022-09-29 13:37:48 -0600

    adding more code

--- Functions/app.js
+++ Functions/app.js
@@ -3,7 +3,12 @@
 // Using LET
 let myName = 'Duvan'; // cannot be a reserved keyword
 
-console.log(myName);
+console.log(myName); // returns "Duvan"
+
+// Using  CONST
+const interestRate = 3.5; // const variales cannot be reassign 
+
+console.log(interestRate);
 
 
 

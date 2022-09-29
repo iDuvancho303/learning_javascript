4307699a0c9d5c1ea26311751cea026eaf55d573
duvancho303 <duvancho303@gmail.com>
2022-09-28 22:00:46 -0600

    adding more code

---
+++ Functions/app.js
@@ -0,0 +1,27 @@
+// VARIABLES
+
+
+
+
+
+// fUNCTIONS
+// Write a simple function
+"use strict";
+
+function colors() {
+    console.log('Hello World :) ');
+}
+
+colors();
+
+// returns "Hello Worlds"
+
+// Write a function with 1 parameter
+let name1 = 'Duvan';
+
+function name(name1) {
+    console.log('Hi my name is' + ' ' + name1);
+
+}
+
+name(name1);

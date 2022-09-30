76dbbe07c4c3d5307f6a09a752827023f7f94274
duvancho303 <duvancho303@gmail.com>
2022-09-29 20:45:33 -0600

    adding more code

--- 01-variables/02-app.js
+++ 01-variables/02-app.js
@@ -5,6 +5,9 @@ let myName = 'Duvan'; // cannot be a reserved keyword
 
 console.log(myName); // returns "Duvan"
 
+let name = 'Mosh'; // string literal
+let age = 30; // number literal
+
 // Using  CONST
 const interestRate = 3.5; // const variales cannot be reassign 
 

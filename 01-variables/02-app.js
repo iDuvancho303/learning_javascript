6cfb1984eeddf7f336863e4d705a4cc90cac4cb4
duvancho303 <duvancho303@gmail.com>
2022-09-29 20:51:56 -0600

    adding more code

--- 01-variables/02-app.js
+++ 01-variables/02-app.js
@@ -7,6 +7,8 @@ console.log(myName); // returns "Duvan"
 
 let name = 'Mosh'; // string literal
 let age = 30; // number literal
+let isApproved = true; // boolean literal
+
 
 // Using  CONST
 const interestRate = 3.5; // const variales cannot be reassign 

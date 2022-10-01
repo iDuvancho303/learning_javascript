0612a9a5d59daff7e70b51c8d987fabd9508291f
Arcila Flores <DUVAN_ARCILA-FLORES@dpsk12.org>
2022-09-30 19:49:34 -0600

    adding more code

--- 02-functions/app.js
+++ 02-functions/app.js
@@ -11,13 +11,20 @@ colors(); // calling the function
 
 // Write a function with 1 parameter
 
-function name(name1) {
+function name(name1) { // one parameter
     console.log('Hi my name is' + ' ' + name1);
 
 }
 
 name('Carlos'); // returns "Hi my name is Duvan";
+// one argument 'Carlos'
 
 
+// Write a function with 2 parameters
 
+function fullName(name, lastName) {
+    console.log('Hi my name is ' + '' + name + ' ' + lastName); // Returns -> Hi my name is Duvan Flores
+}
+
+fullName('Duvan' , 'Flores');
 

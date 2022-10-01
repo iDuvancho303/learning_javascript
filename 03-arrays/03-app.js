36f2ebce3c6a246e78ee0691b03607ae0c7164b1
Arcila Flores <DUVAN_ARCILA-FLORES@dpsk12.org>
2022-09-30 19:05:25 -0600

    adding more code

--- 03-arrays/03-app.js
+++ 03-arrays/03-app.js
@@ -7,9 +7,14 @@ let selectColors = ['green', 'blue']; // array literal, zero or more elements en
 console.log(selectColors[1]); // returns "blue"
 
 // adding another element to an array
+
 selectColors[2] = 'Yellow';
 
 console.log(selectColors[2]); // returns "yellow"
 
+
 // using a property to check the length of an array
+
+console.log(selectColors.length); //returns 3
+
 

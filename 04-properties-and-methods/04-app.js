b40c07d534cabb22cbadcc8f84fb50c81819063d
Arcila Flores <DUVAN_ARCILA-FLORES@dpsk12.org>
2022-10-01 08:30:17 -0600

    adding more code

--- 04-properties-and-methods/04-app.js
+++ 04-properties-and-methods/04-app.js
@@ -1,6 +1,24 @@
 // Properties and Methods
 
 // Properties
+// Using 'length' property
 
 var item = 'teacher computer';
 
+console.log(item.length); // returns -> 16
+
+
+
+
+
+
+
+
+
+// Methods
+// Using 'indexOf'  method
+
+var item2 = 'student ipad';
+
+console.log(item2.indexOf('ip')); // returns -> 8
+ 

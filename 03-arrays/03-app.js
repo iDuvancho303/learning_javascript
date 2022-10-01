1d1d35d009ebf2ceeeefcfe4a1a534141f2c2f61
Arcila Flores <DUVAN_ARCILA-FLORES@dpsk12.org>
2022-09-30 18:17:02 -0600

    adding more code

--- 03-arrays/03-app.js
+++ 03-arrays/03-app.js
@@ -2,6 +2,9 @@
 
 let selectColors = ['green', 'blue']; // array literal, zero or more elements enclosed in brackets
 
+// select/access an element of an array
+
+console.log(selectColors[1]); // returns "blue"
 
 selectColors[2, 3] = 'orange, purple';
 

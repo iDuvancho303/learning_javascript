4e00e33c3df70f5171ee993672ac4d05828b96f6
Arcila Flores <DUVAN_ARCILA-FLORES@dpsk12.org>
2022-10-02 07:48:58 -0600

    adding more code

--- 06-conditionals/06-app.js
+++ 06-conditionals/06-app.js
@@ -3,10 +3,17 @@
 var x = 10;
 var y = 50; 
 
+
+// Using EQUAL statement
+
 if (x == 10) {
     console.log("That\'s correct!"); // returns -> that's correct because x is equal to 10
 }
 
+// Using OR 
+
 if (x == 5 || y == 50) {
-    console.log('Yes, one value is correct'); // returns -> Yes, one value is correct because y is equal to  50
+    console.log('Yes, one value is correct'); // returns -> Yes, one value is correct because at least one statemen is true
 }
+
+

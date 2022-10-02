2dfda39ab763454ff48e73eee0690cf76a7f3cf2
Arcila Flores <DUVAN_ARCILA-FLORES@dpsk12.org>
2022-10-02 07:23:26 -0600

    adding more code

--- 05-operators/05-app.js
+++ 05-operators/05-app.js
@@ -10,3 +10,21 @@ number += 5; // usin plus equal. Returns --> 15
 
 
 // Arithmetic operators
+/*
++ 
+
+-
+
+*
+
+/
+*/
+var number2 = 5 * 10; // returns -> 50
+
+// String operators
+
+var age = 10;
+
+var info = 'My name is Daniel and I am' + ' ' + age + ' ' + 'years old.'
+
+console.log(info);

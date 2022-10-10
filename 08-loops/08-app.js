3f114c761e7c4eeb70e54edc1669aa27ef738481
Duvan <duvancho303@gmail.com>
2022-10-10 17:59:29 -0600

    adding code

--- 08-loops/08-app.js
+++ 08-loops/08-app.js
@@ -2,6 +2,21 @@
 
 // Using "For" loops
 
- for (var i=0; i<15; i++) {
-    
+ for (var i=0; i<=15; i++)  {
+
+// Initializer or starter point
+// condition for when your loop should end
+// incrementor or decrementor -- continiues up or down
  }
+
+ console.log(i);
+
+ var word = "abcd";
+
+ function doubleChar(str) {
+    
+    for (str < 0; str < 4; str++)
+      return word = word[i] + word[i];
+    }
+
+    console.log(word);

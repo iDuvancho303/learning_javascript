88c6d6251bba2df9544b0ff3b3e1b9010ed41d1c
Duvan <duvancho303@gmail.com>
2022-10-11 17:42:58 -0600

    adding code

--- 09-adding-content-dom/09-app.js
+++ 09-adding-content-dom/09-app.js
@@ -1,1 +1,8 @@
  let getElement = document.querySelector(".box1");
+
+ let createNewElement = document.createElement("h2")
+
+ let date = new Date();
+ let currentHour = date.getHours();
+
+ let createMessage;

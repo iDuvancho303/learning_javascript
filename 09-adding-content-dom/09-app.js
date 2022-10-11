4bfa0530e94b16a16e78f943fe4382cb33f09e5e
Duvan <duvancho303@gmail.com>
2022-10-11 16:10:48 -0600

    adding code

--- 09-adding-content-dom/09-app.js
+++ 09-adding-content-dom/09-app.js
@@ -1,1 +1,5 @@
-let box = document.querySelector(".box1")
+let box = document.querySelector(".box1");
+
+let newElement = document.createElement("a");
+
+let newContent = document.createTextNode("This is a link!")

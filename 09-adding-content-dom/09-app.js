aeed9b6894d6f950bd2cecc276e83f123a363c11
Duvan <duvancho303@gmail.com>
2022-10-11 18:33:43 -0600

    adding code

--- 09-adding-content-dom/09-app.js
+++ 09-adding-content-dom/09-app.js
@@ -11,4 +11,14 @@
     createMessage = 'Good Morning:)';
  } else if (currentHour > 10 && currentHour <= 12) {
     createMessage = 'Almost time for lunch';
- } else if 
+ } else if (currentHour > 12 && currentHour <= 18) {
+    createMessage = 'Good Afternoon!';
+ } else if (currentHour > 18 && currentHour <= 22) {
+    createMessage = 'Good Evening :)';
+ } else if (currentHour > 22 && currentHour <= 5) {
+    createMessage = 'Time to go to sleep!';
+ } else {
+    createMessage = 'Are you from a different dimension';
+ }
+
+ 

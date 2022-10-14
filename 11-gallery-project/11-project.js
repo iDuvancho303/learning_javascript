3d83efbd69d5017ba606eae4373d390c1f2ccab3
Duvan <duvancho303@gmail.com>
2022-10-14 10:21:39 -0600

    adding code

---
+++ 11-gallery-project/11-project.js
@@ -0,0 +1,11 @@
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+/* let combinedArray = arr1.concat(arr2); */
+ let combinedArray;
+
+function mergeArrays(arr1, arr2) {
+    let combinedArray = arr1.concat(arr2);
+    return  combinedArray;
+} 
+
